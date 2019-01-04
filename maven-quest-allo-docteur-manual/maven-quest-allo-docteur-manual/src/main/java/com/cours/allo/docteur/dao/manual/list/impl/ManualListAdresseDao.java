f8a38c1b58b7beda4ea0e3cecdc1d6556876ab8e
Segolene FATIER <fatier-segolene@outlook.com>
2019-01-04 11:52:00 +0100

    ManualListUtilisateurDao & ManualListAdresseDao complété

--- maven-quest-allo-docteur-manual/maven-quest-allo-docteur-manual/src/main/java/com/cours/allo/docteur/dao/manual/list/impl/ManualListAdresseDao.java
+++ maven-quest-allo-docteur-manual/maven-quest-allo-docteur-manual/src/main/java/com/cours/allo/docteur/dao/manual/list/impl/ManualListAdresseDao.java
@@ -8,7 +8,9 @@ package com.cours.allo.docteur.dao.manual.list.impl;
 import com.cours.allo.docteur.dao.DataSource;
 import com.cours.allo.docteur.dao.IAdresseDao;
 import com.cours.allo.docteur.dao.entities.Adresse;
+import com.cours.allo.docteur.dao.entities.Utilisateur;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -37,36 +39,103 @@ public class ManualListAdresseDao extends AbstractListDao<Adresse> implements IA
 
     @Override
     public List<Adresse> findAllAdresses() {
-        return null;
+    	List<Adresse> lst_adresse = new ArrayList<Adresse>();
+    	
+    	lst_adresse = DataSource.getInstance().getAdressesListDataSource();
+    	
+        return lst_adresse;
     }
 
     @Override
     public Adresse findAdresseById(int idAdresse) {
-        return null;
+    	List<Adresse> lst =  findAllAdresses();
+    	Adresse result = new Adresse();
+    	
+        for(Adresse u : lst){
+    		if (u.getIdUtilisateur().equals(idAdresse)) {
+    			result = u;
+    			break;
+    		}
+    	}
+    	
+        return result;
     }
 
     @Override
     public List<Adresse> findAdressesByVille(String ville) {
-        return null;
+    	List<Adresse> lst =  findAllAdresses();
+    	List<Adresse> result = new ArrayList<Adresse>();
+    	
+        for(Adresse u : lst){
+    		if (u.getVille().equals(ville)) {
+    			result.add(u);
+    		}
+    	}
+    	return result;
     }
 
     @Override
     public List<Adresse> findAdressesByCodePostal(String codePostal) {
-        return null;
+    	List<Adresse> lst =  findAllAdresses();
+    	List<Adresse> result = new ArrayList<Adresse>();
+    	
+        for(Adresse u : lst){
+    		if (u.getCodePostal().equals(codePostal)) {
+    			result.add(u);
+    		}
+    	}
+    	return result;
     }
 
     @Override
     public Adresse createAdresse(Adresse adresse) {
-        return null;
+    	try {
+    		DataSource.getInstance().getAdressesListDataSource().add(adresse);
+    		return adresse;
+    	}catch(Exception ex) {
+    		return null;
+    	}    	       
     }
 
     @Override
-    public Adresse updateAdresse(Adresse adresse) {
-        return null;
+    public Adresse updateAdresse(Adresse adresse) {     
+    	try {
+    		   
+    		for(int i =0; i < DataSource.getInstance().getAdressesListDataSource().size(); i++){
+    			Adresse a = DataSource.getInstance().getAdressesListDataSource().get(i);
+            	 if (a.getIdAdresse() == adresse.getIdAdresse()) {
+            			a.setRue(adresse.getRue()) ;           	 
+    	            	a.setCodePostal(adresse.getCodePostal());
+    	            	a.setVille(adresse.getVille());
+    	            	a.setPays(adresse.getPays());
+    	            	a.setVersion(adresse.getVersion());
+    	            	a.setIdUtilisateur(adresse.getIdUtilisateur());
+	            	break;
+            	 }
+         	 }      
+    		    		   
+	    	return adresse;
+    	}catch(Exception ex) {
+    		return null;
+    	}
+        
     }
 
     @Override
     public boolean deleteAdresse(Adresse adresse) {
-        return false;
+    	try {
+    		List<Adresse> lst =  DataSource.getInstance().getAdressesListDataSource();
+        	
+    		for(int i =0; i < lst.size(); i++){
+    			Adresse a = lst.get(i);
+            	 if (a.getIdAdresse() == adresse.getIdAdresse())
+         	    	lst.remove(i);
+            	 	break;
+         	 }      	
+        	
+    		return true;
+    	}catch(Exception ex) {
+    		return false;
+    	}    
     }
 }
--- maven-quest-allo-docteur-manual/maven-quest-allo-docteur-manual/src/main/java/com/cours/allo/docteur/dao/manual/list/impl/ManualListUtilisateurDao.java
+++ maven-quest-allo-docteur-manual/maven-quest-allo-docteur-manual/src/main/java/com/cours/allo/docteur/dao/manual/list/impl/ManualListUtilisateurDao.java
@@ -7,7 +7,11 @@ package com.cours.allo.docteur.dao.manual.list.impl;
 
 import com.cours.allo.docteur.dao.DataSource;
 import com.cours.allo.docteur.dao.IUtilisateurDao;
+import com.cours.allo.docteur.dao.entities.Adresse;
 import com.cours.allo.docteur.dao.entities.Utilisateur;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -36,46 +40,136 @@ public class ManualListUtilisateurDao extends AbstractListDao<Utilisateur> imple
     
     @Override
     public List<Utilisateur> findAllUtilisateurs() {
-        return null;
+    	
+    	List<Utilisateur> lst_utilisateur = new ArrayList<Utilisateur>();
+    	
+    	lst_utilisateur = DataSource.getInstance().getUtilisateursListDataSource();
+    	
+        return lst_utilisateur;
     }
 
     @Override
     public Utilisateur findUtilisateurById(int idUtilisateur) {
-        return null;
+    	List<Utilisateur> lst =  findAllUtilisateurs();
+    	Utilisateur result = new Utilisateur();
+    	
+        for(Utilisateur u : lst){
+    		if (u.getIdUtilisateur().equals(idUtilisateur)) {
+    			result = u;
+    			break;
+    		}
+    	}
+    	
+        return result;
     }
 
     @Override
     public List<Utilisateur> findUtilisateurByIdentifiant(String identifiant) {
-        return null;
+    	List<Utilisateur> lst =  findAllUtilisateurs();
+    	List<Utilisateur> result = new ArrayList<Utilisateur>();
+    	
+        for(Utilisateur u : lst){
+    		if (u.getIdentifiant().equals(identifiant)) {
+    			result.add(u);
+    		}
+    	}
+    	return result;
     }
 
     @Override
     public List<Utilisateur> findUtilisateursByPrenom(String prenom) {
-        return null;
+    	List<Utilisateur> lst =  findAllUtilisateurs();
+    	List<Utilisateur> result = new ArrayList<Utilisateur>();
+    	
+        for(Utilisateur u : lst){
+    		if (u.getPrenom().equals(prenom)) {
+    			result.add(u);
+    		}
+    	}
+    	return result;
     }
 
     @Override
     public List<Utilisateur> findUtilisateursByNom(String nom) {
-        return null;
+    	List<Utilisateur> lst =  findAllUtilisateurs();
+    	List<Utilisateur> result = new ArrayList<Utilisateur>();
+    	
+        for(Utilisateur u : lst){
+    		if (u.getNom().equals(nom)) {
+    			result.add(u);
+    		}
+    	}
+    	return result;
     }
 
     @Override
     public List<Utilisateur> findUtilisateursByCodePostal(String codePostal) {
-        return null;
+    	List<Utilisateur> lst =  findAllUtilisateurs();
+    	List<Utilisateur> result = new ArrayList<Utilisateur>();
+    	
+        for(Utilisateur u : lst){
+        	for (Adresse a : u.getAdresses()) {
+	    		if (a.getCodePostal().equals(codePostal)) {
+	    			result.add(u);
+	    			break;
+	    		}
+        	}
+    	}
+    	return result;
     }
 
     @Override
     public Utilisateur createUtilisateur(Utilisateur user) {
-        return null;
+    	try {
+    		DataSource.getInstance().getUtilisateursListDataSource().add(user);
+    		return user;
+    	}catch(Exception ex) {
+    		return null;
+    	}    
     }
 
     @Override
     public Utilisateur updateUtilisateur(Utilisateur user) {
-        return null;
+    	
+    	try {
+    		   
+    		for(int i =0; i < DataSource.getInstance().getUtilisateursListDataSource().size(); i++){
+            	Utilisateur u = DataSource.getInstance().getUtilisateursListDataSource().get(i);
+            	 if (u.getIdUtilisateur() == user.getIdUtilisateur()) {
+            		u.setCivilite(user.getCivilite()) ;           	 
+	            	u.setPrenom(user.getPrenom());
+	            	u.setNom(user.getNom());
+	            	u.setIdentifiant(user.getMotPasse());
+	            	u.setNumeroTelephone(user.getNumeroTelephone());
+	            	u.setDateNaissance(user.getDateNaissance());
+	            	u.setDateCreation(user.getDateCreation());
+		     		u.setDateModification(user.getDateModification());
+		     		u.setActif(user.isActif());
+		     		u.setMarquerEffacer(user.isMarquerEffacer());
+		     		u.setVersion(user.getVersion());
+	            	break;
+            	 }
+         	 }      
+    		    		   
+	    	return user;
+    	}catch(Exception ex) {
+    		return null;
+    	}
     }
 
     @Override
     public boolean deleteUtilisateur(Utilisateur user) {
-        return false;
+    	try {    		
+    		for(int i =0; i < DataSource.getInstance().getUtilisateursListDataSource().size(); i++){
+                	Utilisateur u = DataSource.getInstance().getUtilisateursListDataSource().get(i);
+            	 if (u.getIdUtilisateur() == user.getIdUtilisateur())
+            		 DataSource.getInstance().getUtilisateursListDataSource().remove(i);
+            	 	break;
+         	 }      	
+        	
+    		return true;
+    	}catch(Exception ex) {
+    		return false;
+    	}    
     }
 }
